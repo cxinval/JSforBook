4eff4ae9e8f46bf9012370557b2053550cb436df
cxinval <cxinval7@mail.ru>
2022-07-19 11:25:25 +0300

    02:13:00

---
+++ Lesson_3/3.8.js
@@ -0,0 +1,14 @@
+function circleLength(d){
+    let l;
+    l = d * Math.PI
+    return l
+    
+}
+window.document.write('<tr><td>2</td><br>')
+window.document.write('<td>', circleLength(2),'</td></tr>')
+window.document.write('<tr><td>5</td><br>')
+window.document.write('<td>', circleLength(5),'</td></tr>')
+window.document.write('<tr><td>10</td><br>')
+window.document.write('<td>', circleLength(10),'</td></tr>')
+window.document.write('<tr><td>20</td><br>')
+window.document.write('<td>', circleLength(20),'</td></tr>')
